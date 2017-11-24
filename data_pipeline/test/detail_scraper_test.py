4e3f1200cc4d6e61701c8d4b3efb465802e1c11d
Shengjie Xu <shengjix12@gmail.com>
2017-11-24 10:06:37 -0800

    detail scraper test pasted

--- data_pipeline/test/detail_scraper_test.py
+++ data_pipeline/test/detail_scraper_test.py
@@ -8,11 +8,30 @@ from detail import DetailScraper
 
 
 class DetailScraperTestCase(unittest.TestCase):
+    def setUp(self):
+        self.tasks = [{"url": "https://orangecounty.craigslist.org/apa/d/spacious-studio-fully/6396070267.html",
+                       "region": "orangecounty",
+                       "category": "apa"}, {
+                        "url": "https://orangecounty.craigslist.org/apa/d/title/6398911602.html",
+                        "region": "orangecounty",
+                        "category": "apa"
+                       }, {
+                        "url": "https://newyork.craigslist.org/brk/abo/d/perfect-3brno/6390308280.html",
+                        "region": "newyork",
+                        "category": "aap"
+                       }, {
+                        "url": "https://losangeles.craigslist.org/sfv/apa/d/modern-remodeled-unit-in/6368859107.html",
+                        "region": "losangeles",
+                        "category": "apa"
+                       }]
+
     def test_get_details(self):
         logger = logging.getLogger(__name__)
-        scraper = DetailScraper("https://orangecounty.craigslist.org/apa/d/spacious-studio-fully/6396070267.html", "orangecounty", "apa")
-        detail = scraper.get_details()
-        self.assertEqual(detail["size"], "432ft2")
+        for task in self.tasks:
+            scraper = DetailScraper(url=task["url"], region=task["region"], category=task["category"])
+            logger.info("Testing region=%s, url=%s", task["region"], task["url"])
+            detail = scraper.get_details()
+            self.assertEqual(detail["url"], task["url"])
 
 
 if __name__ == "__main__":
