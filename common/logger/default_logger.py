849d8dcde8d16e203f881fdb05664139f9779d04
Shengjie Xu <shengjix12@gmail.com>
2017-11-27 10:15:01 -0800

    default dual logger=rotatefilehandler+streamhandler

--- common/logger/default_logger.py
+++ common/logger/default_logger.py
@@ -3,27 +3,33 @@
 """Config default loggers"""
 
 import logging
+import logging.handlers
 
 
-def set_default_dual_logger(filename="default.log", filemode="a"):
+def set_default_dual_logger(filename="default.log"):
     """
     Default: log DEBUG and higher message to the file, "a" mode,
              log INFO and higher message to the console
     """
 
-    logging.basicConfig(level=logging.DEBUG,
-                        format="%(asctime)s %(name)-12s %(levelname)-8s %(message)s",
-                        datefmt="%m-%d %H:%M",
-                        filename=filename,
-                        filemode=filemode)
+    root_logger = logging.getLogger()
+    root_logger.setLevel(logging.DEBUG)
+
+    file_handler = logging.handlers.RotatingFileHandler(filename,
+                                                        maxBytes=100000000,
+                                                        backupCount=5)
+    file_handler.setLevel(logging.DEBUG)
+    file_fmter = logging.Formatter(fmt="%(asctime)s %(name)-12s %(levelname)-8s %(message)s",
+                                   datefmt="%m-%d %H:%M")
+    file_handler.setFormatter(file_fmter)
 
     console_handler = logging.StreamHandler()
     console_handler.setLevel(logging.INFO)
+    console_fmter = logging.Formatter(fmt="%(name)-12s %(levelname)-8s %(message)s")
+    console_handler.setFormatter(console_fmter)
 
-    fmter = logging.Formatter(fmt="%(name)-12s %(levelname)-8s %(message)s")
-    console_handler.setFormatter(fmter)
-
-    logging.getLogger("").addHandler(console_handler)
+    root_logger.addHandler(file_handler)
+    root_logger.addHandler(console_handler)
 
 
 def set_default_console_logger():
@@ -33,4 +39,3 @@ def set_default_console_logger():
 
     logging.basicConfig(level=logging.INFO,
                         format="%(name)-12s %(levelname)-8s %(message)s")
-
